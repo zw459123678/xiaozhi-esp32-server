1384652dd93eff39383ee2ac5ca2f6d6d2c41432
hrz <1710360675@qq.com>
2025-02-15 09:56:24 +0800

    no message

---
+++ core/providers/tts/siliconflow.py
@@ -0,0 +1,41 @@
+import os
+import uuid
+import json
+import base64
+import requests
+from datetime import datetime
+from core.providers.tts.base import TTSProviderBase
+
+
+class TTSProvider(TTSProviderBase):
+    def __init__(self, config, delete_audio_file):
+        super().__init__(config, delete_audio_file)
+        self.model = config.get("model")
+        self.access_token = config.get("access_token")
+        self.voice = config.get("voice")
+        self.response_format = config.get("response_format")
+        self.sample_rate = config.get("sample_rate")
+        self.speed = config.get("speed")
+        self.gain = config.get("gain")
+
+        self.host = "api.siliconflow.cn"
+        self.api_url = f"https://{self.host}/v1/audio/speech"
+
+    def generate_filename(self, extension=".wav"):
+        return os.path.join(self.output_file, f"tts-{datetime.now().date()}@{uuid.uuid4().hex}{extension}")
+
+    async def text_to_speak(self, text, output_file):
+        request_json = {
+            "model": self.model,
+            "input": text,
+            "voice": self.voice,
+            "response_format": self.response_format,
+        }
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json"
+        }
+        response = requests.request("POST", self.api_url, json=request_json, headers=headers)
+        data = response.content
+        file_to_save = open(output_file, "wb")
+        file_to_save.write(data)
