5264bfd25835b6015ec736b8b169a44ecd24c116
kalicyh <34980061+kaliCYH@users.noreply.github.com>
2025-02-20 10:56:51 +0800

    🎈 perf: 优化开发
    dockerfile移除网络下载
    添加前端说明文档
    修改静态文件调用

--- manager/http_server.py
+++ manager/http_server.py
@@ -38,7 +38,7 @@ class WebUI:
         self.session_manager = SessionManager()
         
         # 添加静态文件路径
-        self.static_path = os.path.join(root_dir, 'manager', 'static', 'webui')
+        self.static_path = os.path.join(root_dir, 'ZhiKongTaiWeb', 'dist')
         
         self.setup_routes()
         self.setup_cors()
