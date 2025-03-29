70b6efe922df31755ebe61d5ab79e8448d9e398e
caixypromise <caixypromised@gmail.com>
2025-03-29 16:02:30 +0800

    feature: 新增实现OTA功能——实现设备激活、前端输入激活码验证并保存设备。

--- main/manager-api/src/main/java/xiaozhi/modules/device/service/DeviceService.java
+++ main/manager-api/src/main/java/xiaozhi/modules/device/service/DeviceService.java
@@ -2,12 +2,22 @@ package xiaozhi.modules.device.service;
 
 import xiaozhi.common.page.PageData;
 import xiaozhi.modules.device.dto.DeviceHeaderDTO;
+import xiaozhi.modules.device.dto.DeviceReportReqDTO;
+import xiaozhi.modules.device.dto.DeviceReportRespDTO;
 import xiaozhi.modules.device.entity.DeviceEntity;
 
 import java.util.List;
 import java.util.Map;
 
 public interface DeviceService {
+
+    /**
+    * 根据Mac地址获取设备信息
+    */
+    DeviceEntity getDeviceById(String macAddress);
+
+    DeviceReportRespDTO checkDeviceActive(String macAddress, String deviceId, String clientId, DeviceReportReqDTO deviceReport);
+
     DeviceEntity bindDevice(Long userId, DeviceHeaderDTO deviceHeader);
     
     List<DeviceEntity> getUserDevices(Long userId);
@@ -15,4 +25,6 @@ public interface DeviceService {
     void unbindDevice(Long userId, Long deviceId);
     
     PageData<DeviceEntity> adminDeviceList(Map<String, Object> params);
+
+    Boolean deviceActivation(String activationCode);
 }
