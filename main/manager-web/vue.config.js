20c1f0576b0710850f55853996c3da6a6e1837a1
huangjunsen <951434130@qq.com>
2025-03-31 19:52:01 +0800

    build(环境变量): :package: 去除.env忽略、修改请求地址、控制台名称通过env获取
    修改了环境变量的读取方式
    
    调整了请求地址配置
    
    控制台名称通过 .env 获取

--- main/manager-web/vue.config.js
+++ main/manager-web/vue.config.js
@@ -3,22 +3,30 @@ const dotenv = require('dotenv');
 
 // 确保加载 .env 文件
 dotenv.config();
+console.log("base_url",process.env.VUE_APP_API_BASE_URL);
 
 module.exports = defineConfig({
-    devServer: {
-      // Bug 修复：将代理配置为环境变量中定义的 API 基础 URL
-      port: 8001, // 指定端口为 8001
-      proxy: {
-        '/xiaozhi-esp32-api': {
-          target: process.env.VUE_APP_API_BASE_URL || 'http://localhost:8002', // 后端 API 的基础 URL
-          changeOrigin: true, // 允许跨域
-          // pathRewrite: {
-          //   '^/api': '', // 路径重写
-          // },
-        },
-        },
-        client: {
-          overlay: false,
+  chainWebpack: config => {
+    config.plugin('html')
+      .tap(args => {
+        // 将 VUE_APP_TITLE 从环境变量中注入到 HTML 中
+        args[0].title = process.env.VUE_APP_TITLE || '小智-智控台';
+        return args;
+      });
+  },
+  devServer: {
+    port: 8001, // 指定端口为 8001
+    proxy: {
+      '/xiaozhi-esp32-api': {
+        target: process.env.VUE_APP_API_BASE_URL, // 后端 API 的基础 URL
+        changeOrigin: true, // 允许跨域
+        pathRewrite: {
+          '^/api': '', // 路径重写
         },
       },
+    },
+    client: {
+      overlay: false, // 不显示 webpack 错误覆盖层
+    },
+  },
 });
