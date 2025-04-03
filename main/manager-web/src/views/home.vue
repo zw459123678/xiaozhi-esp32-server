b042a34726c09b4f672fa43a5ead927d6ff83aef
hrz <1710360675@qq.com>
2025-04-03 17:40:53 +0800

    优化登录页大背景图片文件，加快页面加载 (#660)
    * update:优化说明文档
    
    * update：优化登录页大背景图片文件，加快页面加载

--- main/manager-web/src/views/home.vue
+++ main/manager-web/src/views/home.vue
@@ -38,7 +38,7 @@
             />
           </div>
         </div>
-        <div style="font-size: 12px;font-weight: 400;margin-top: auto;padding-top: 30px;color: #979db1;">
+        <div class="copyright">
           ©2025 xiaozhi-esp32-server
         </div>
         <AddWisdomBodyDialog :visible.sync="addDeviceDialogVisible" @confirm="handleWisdomBodyAdded" />
