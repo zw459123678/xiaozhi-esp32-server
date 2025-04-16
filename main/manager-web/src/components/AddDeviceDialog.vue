56959808a95bb5b8bfcfdf195a80cc70c3a62927
Ran_Chen <1908198662@qq.com>
2025-04-16 11:17:02 +0800

    解决新增设备按钮绑定问题，优化“角色配置”页面

--- main/manager-web/src/components/AddDeviceDialog.vue
+++ main/manager-web/src/components/AddDeviceDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-dialog :visible.sync="visible" width="400px" center>
+  <el-dialog :visible="visible" @close="handleClose" width="400px" center>
     <div
       style="margin: 0 10px 10px;display: flex;align-items: center;gap: 10px;font-weight: 700;font-size: 20px;text-align: left;color: #3d4566;">
       <div
@@ -79,7 +79,10 @@ export default {
     cancel() {
       this.$emit('update:visible', false)
       this.deviceCode = ""
-    }
+    },
+    handleClose() {
+      this.$emit('update:visible', false);
+    },
   }
 }
 </script>
