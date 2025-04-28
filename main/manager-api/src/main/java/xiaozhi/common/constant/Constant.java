90a31debd1e4d07feeecbfb2da1e4c892c12d1e7
hrz <1710360675@qq.com>
2025-04-28 11:56:17 +0800

    update:修改版本号

--- main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
+++ main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
@@ -177,5 +177,5 @@ public interface Constant {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.3.12";
+    public static final String VERSION = "0.3.13";
 }
--- main/xiaozhi-server/config/logger.py
+++ main/xiaozhi-server/config/logger.py
@@ -3,7 +3,7 @@ import sys
 from loguru import logger
 from config.config_loader import load_config
 
-SERVER_VERSION = "0.3.12"
+SERVER_VERSION = "0.3.13"
 
 
 def get_module_abbreviation(module_name, module_dict):
