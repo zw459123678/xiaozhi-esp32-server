c51c8b4af0cf0affa397f3b97bb898b22fb0f266
ljwwd2 <1205484020@qq.com>
2025-05-16 01:42:19 +0800

    供应器管理-增删改查api

---
+++ main/manager-api/src/main/java/xiaozhi/modules/model/controller/ModelProviderController.java
@@ -0,0 +1,57 @@
+package xiaozhi.modules.model.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import xiaozhi.common.annotation.UpdateGroup;
+import xiaozhi.common.page.PageData;
+import xiaozhi.common.utils.Result;
+import xiaozhi.modules.model.dto.ModelProviderDTO;
+import xiaozhi.modules.model.service.ModelProviderService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/models/provider")
+@Tag(name = "模型配置")
+public class ModelProviderController {
+
+    private final ModelProviderService modelProviderService;
+
+    @GetMapping
+    @Operation(summary = "获取模型配置列表")
+    @RequiresPermissions("sys:role:superAdmin")
+    public Result<PageData<ModelProviderDTO>> getListPage(@RequestBody ModelProviderDTO modelProviderDTO,
+                                                             @RequestParam(required = true, defaultValue = "0") String page,
+                                                             @RequestParam(required = true, defaultValue = "10") String limit) {
+        return new Result<PageData<ModelProviderDTO>>()
+                .ok(modelProviderService.getListPage(modelProviderDTO, page, limit));
+    }
+
+    @PostMapping
+    @Operation(summary = "获取模型配置列表")
+    @RequiresPermissions("sys:role:superAdmin")
+    public Result<ModelProviderDTO> add(@RequestBody @Validated ModelProviderDTO modelProviderDTO) {
+        ModelProviderDTO resp = modelProviderService.add(modelProviderDTO);
+        return new Result<ModelProviderDTO>().ok(resp);
+    }
+
+    @PutMapping
+    @Operation(summary = "获取模型配置列表")
+    @RequiresPermissions("sys:role:superAdmin")
+    public Result<ModelProviderDTO> edit(@RequestBody @Validated(UpdateGroup.class) ModelProviderDTO modelProviderDTO) {
+        ModelProviderDTO resp = modelProviderService.edit(modelProviderDTO);
+        return new Result<ModelProviderDTO>().ok(resp);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "获取模型配置列表")
+    @RequiresPermissions("sys:role:superAdmin")
+    public Result<Void> delete(@PathVariable String id) {
+        modelProviderService.delete(id);
+        return new Result<>();
+    }
+
+}
--- main/manager-api/src/main/java/xiaozhi/modules/model/service/impl/ModelProviderServiceImpl.java
+++ main/manager-api/src/main/java/xiaozhi/modules/model/service/impl/ModelProviderServiceImpl.java
@@ -1,19 +1,28 @@
 package xiaozhi.modules.model.service.impl;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 
 import lombok.AllArgsConstructor;
+import xiaozhi.common.constant.Constant;
+import xiaozhi.common.exception.RenException;
+import xiaozhi.common.page.PageData;
 import xiaozhi.common.service.impl.BaseServiceImpl;
+import xiaozhi.common.user.UserDetail;
 import xiaozhi.common.utils.ConvertUtils;
 import xiaozhi.modules.model.dao.ModelProviderDao;
 import xiaozhi.modules.model.dto.ModelProviderDTO;
 import xiaozhi.modules.model.entity.ModelProviderEntity;
 import xiaozhi.modules.model.service.ModelProviderService;
+import xiaozhi.modules.security.user.SecurityUser;
 
 @Service
 @AllArgsConstructor
@@ -32,18 +41,54 @@ public class ModelProviderServiceImpl extends BaseServiceImpl<ModelProviderDao, 
     }
 
     @Override
-    public ModelProviderDTO add(ModelProviderEntity modelProviderEntity) {
-        return null;
+    public PageData<ModelProviderDTO> getListPage(ModelProviderDTO modelProviderEntity, String page, String limit) {
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put(Constant.PAGE, page);
+        params.put(Constant.LIMIT, limit);
+
+        IPage<ModelProviderEntity> pageParam = getPage(params, "model_type", true);
+
+        QueryWrapper<ModelProviderEntity> wrapper = new QueryWrapper<ModelProviderEntity>();
+
+        if (StringUtils.isNotBlank(modelProviderEntity.getModelType())) {
+            wrapper.eq("model_type", modelProviderEntity.getModelType());
+        }
+
+        if (StringUtils.isNotBlank(modelProviderEntity.getName())) {
+            wrapper.like("name", "%" + modelProviderEntity.getName() + "%");
+        }
+        return getPageData(modelProviderDao.selectPage(pageParam, wrapper), ModelProviderDTO.class);
     }
 
     @Override
-    public ModelProviderDTO edit(ModelProviderEntity modelProviderEntity) {
-        return null;
+    public ModelProviderDTO add(ModelProviderDTO modelProviderEntity) {
+        UserDetail user = SecurityUser.getUser();
+        modelProviderEntity.setCreator(user.getId());
+        modelProviderEntity.setUpdater(user.getId());
+        modelProviderEntity.setCreateDate(new Date());
+        modelProviderEntity.setUpdateDate(new Date());
+        if (modelProviderDao.insert(ConvertUtils.sourceToTarget(modelProviderEntity, ModelProviderEntity.class)) == 0) {
+            throw new RenException("新增数据失败");
+        }
+
+        return ConvertUtils.sourceToTarget(modelProviderEntity, ModelProviderDTO.class);
     }
 
     @Override
-    public void delete() {
+    public ModelProviderDTO edit(ModelProviderDTO modelProviderEntity) {
+        UserDetail user = SecurityUser.getUser();
+        modelProviderEntity.setUpdater(user.getId());
+        modelProviderEntity.setUpdateDate(new Date());
+        if (modelProviderDao.updateById(ConvertUtils.sourceToTarget(modelProviderEntity, ModelProviderEntity.class)) == 0) {
+            throw new RenException("修改数据失败");
+        }
+        return ConvertUtils.sourceToTarget(modelProviderEntity, ModelProviderDTO.class);
+    }
 
+    @Override
+    public void delete(String id) {
+        modelProviderDao.deleteById(id);
     }
 
     @Override
