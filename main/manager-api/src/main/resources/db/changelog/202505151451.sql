2ecce6485cad764d7ed10fbcf4bd6230794ed65b
hrz <1710360675@qq.com>
2025-05-22 11:26:00 +0800

    update:增加自定义的TTS接口服务的使用说明

--- main/manager-api/src/main/resources/db/changelog/202505151451.sql
+++ main/manager-api/src/main/resources/db/changelog/202505151451.sql
@@ -7,12 +7,12 @@ where `id` = 'SYSTEM_TTS_custom';
 UPDATE `ai_model_config` SET
 `doc_link` = NULL,
 `remark` = '自定义TTS配置说明：
-1. 支持自定义TTS接口服务
-2. 使用GET/POST方式请求
-3. 需要网络连接
-4. 输出文件保存在tmp/目录
+1. 自定义的TTS接口服务，请求参数可自定义，可接入众多TTS服务
+2. 以本地部署的KokoroTTS为例
+3. 如果只有cpu运行：docker run -p 8880:8880 ghcr.io/remsky/kokoro-fastapi-cpu:latest
+4. 如果只有gpu运行：docker run --gpus all -p 8880:8880 ghcr.io/remsky/kokoro-fastapi-gpu:latest
 配置说明：
 1. 在params中配置请求参数,使用JSON格式
-   例如：{"text": "{prompt_text}", "voice": "{voice}", "speed": "{speed}"}
+   例如KokoroTTS：{ "input": "{prompt_text}", "speed": 1, "voice": "zm_yunxi", "stream": true, "download_format": "mp3", "response_format": "mp3", "return_download_link": true }
 2. 在headers中配置请求头
 3. 设置返回音频格式' WHERE `id` = 'TTS_CustomTTS';
