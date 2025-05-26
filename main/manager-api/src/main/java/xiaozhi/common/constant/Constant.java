0c8e943d1b3aaf968713e6b63cdb58ab8e97e1c0
hrz <1710360675@qq.com>
2025-05-26 16:18:35 +0800

    update:修改版本号

--- main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
+++ main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
@@ -227,7 +227,7 @@ public interface Constant {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.4.4";
+    public static final String VERSION = "0.5.1";
 
     /**
      * 无效固件URL
