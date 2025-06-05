492e0b952e58795ec2845e522dd08516bf5a94bc
hrz <1710360675@qq.com>
2025-06-05 09:03:17 +0800

    update:删除无效引用

--- main/manager-api/src/main/java/xiaozhi/modules/sys/enums/ServerActionEnum.java
+++ main/manager-api/src/main/java/xiaozhi/modules/sys/enums/ServerActionEnum.java
@@ -2,36 +2,29 @@ package xiaozhi.modules.sys.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import xiaozhi.common.exception.RenException;
 
 /**
  * 服务端动作枚举
  */
-public enum ServerActionEnum
-{
+public enum ServerActionEnum {
     RESTART("restart"),
     UPDATE_CONFIG("update_config");
 
     private final String value;
 
-    ServerActionEnum(String value)
-    {
+    ServerActionEnum(String value) {
         this.value = value;
     }
 
     @JsonValue
-    public String getValue()
-    {
+    public String getValue() {
         return value;
     }
 
     @JsonCreator
-    public static ServerActionEnum fromValue(String value)
-    {
-        for (ServerActionEnum action : ServerActionEnum.values())
-        {
-            if (action.value.equalsIgnoreCase(value))
-            {
+    public static ServerActionEnum fromValue(String value) {
+        for (ServerActionEnum action : ServerActionEnum.values()) {
+            if (action.value.equalsIgnoreCase(value)) {
                 return action;
             }
         }
