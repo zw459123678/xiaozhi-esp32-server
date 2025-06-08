3d1768100f46b1419a1b8e13ed2120ea24857939
CGD <3030332422@qq.com>
2025-06-08 15:40:18 +0800

    fix:使用“clear_markdown”方法修复“使用火山流式TTS中markdown 语法被 TTS 读出”的问题

--- main/xiaozhi-server/core/providers/tts/huoshan_double_stream.py
+++ main/xiaozhi-server/core/providers/tts/huoshan_double_stream.py
@@ -5,6 +5,7 @@ import queue
 import asyncio
 import traceback
 import websockets
+from core.utils.tts import MarkdownCleaner
 from config.logger import setup_logging
 from core.utils import opus_encoder_utils
 from core.utils.util import check_model_key
@@ -266,8 +267,12 @@ class TTSProvider(TTSProviderBase):
                 await handleAbortMessage(self.conn)
                 logger.bind(tag=TAG).error(f"WebSocket连接不存在，终止发送文本")
                 return
+            
+            #  过滤Markdown
+            filtered_text = MarkdownCleaner.clean_markdown(text)
+
             # 发送文本
-            await self.send_text(self.voice, text, self.conn.sentence_id)
+            await self.send_text(self.voice, filtered_text, self.conn.sentence_id)
             return
         except Exception as e:
             logger.bind(tag=TAG).error(f"发送TTS文本失败: {str(e)}")
