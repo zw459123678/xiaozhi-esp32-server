04b132816c624de2e4c711f4a4180028cd99f561
hrz <1710360675@qq.com>
2025-07-01 18:16:21 +0800

    update:修改版本号
    * update:优化mcp工具较多时的现实
    
    * update:修改版本号

--- main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
+++ main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
@@ -232,7 +232,7 @@ public interface Constant {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.6.1";
+    public static final String VERSION = "0.6.2";
 
     /**
      * 无效固件URL
--- main/xiaozhi-server/config/logger.py
+++ main/xiaozhi-server/config/logger.py
@@ -5,7 +5,7 @@ from config.config_loader import load_config
 from config.settings import check_config_file
 from datetime import datetime
 
-SERVER_VERSION = "0.6.1"
+SERVER_VERSION = "0.6.2"
 _logger_initialized = False
 
 
