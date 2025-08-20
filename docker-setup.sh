31f596f96c905661db8457837cb28a2fd040b2dd
hrz <1710360675@qq.com>
2025-08-20 14:41:22 +0800

    Update docker-setup.sh
    全模块部署，OTA地址是在8002端口

--- docker-setup.sh
+++ docker-setup.sh
@@ -409,7 +409,7 @@ LOCAL_IP=$(hostname -I | awk '{print $1}')
 whiptail --title "安装完成！" --msgbox "\
 服务端相关地址如下：\n\
 管理后台访问地址: http://$LOCAL_IP:8002\n\
-OTA 地址: http://$LOCAL_IP:8003/xiaozhi/ota/\n\
+OTA 地址: http://$LOCAL_IP:8002/xiaozhi/ota/\n\
 视觉分析接口地址: http://$LOCAL_IP:8003/mcp/vision/explain\n\
 WebSocket 地址: ws://$LOCAL_IP:8000/xiaozhi/v1/\n\
 \n安装完毕！感谢您的使用！\n按Enter键退出..." 16 70
